82d80981fd5846d862f1498e234da9f4b2212b2d
Adina <adinaerkinbekkyzy17@gmail.com>
2025-10-16 00:24:20 +0600

    Initial commit

---
+++ src/Phone.java
@@ -0,0 +1,16 @@
+import static javax.swing.text.html.HTML.Tag.S;
+public class Phone {
+    String name;
+    String model;
+    int year ;
+    int price;
+    int storage;
+    String colour;
+
+
+    public  void toTakeaPhoto(){
+        System.out.println(" toTakeaPhoto");
+    }
+
+
+}
